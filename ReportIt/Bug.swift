4b5a4ec028a835d2d12c1ab086946f65cb87496b
DaniaAlogla <daniaalogla@gmail.com>
2024-04-04 19:17:56 +0300

    move 'Bug' to its own file.

---
+++ ReportIt/Bug.swift
@@ -0,0 +1,14 @@
+//
+//  Bug.swift
+//  ReportIt
+//
+//  Created by Dania Alogla on 25/09/1445 AH.
+//
+
+import Foundation
+import SwiftUI
+
+struct Bug {
+    var description: String
+    var image: UIImage?
+}
