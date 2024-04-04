1d8618886bc5f1f1b108e6afc4adf244536da640
DaniaAlogla <daniaalogla@gmail.com>
2024-04-04 21:15:54 +0300

    adding a function to submit the bug.

--- ReportIt/BugSubmissionView.swift
+++ ReportIt/BugSubmissionView.swift
@@ -6,8 +6,30 @@
 //
 
 import SwiftUI
+import bug_api
 
 public class BugSubmissionViewModel: ObservableObject {
+    
+    public func submitBug(_ bug: Bug) {
+        convertImageToData(bug.image!) { result in
+            switch result {
+            case let .success(data):
+                let imageData = data
+                FirebaseStorageManager.shared.getImageURL(for: imageData) { result in
+                    switch result {
+                    case let .success(url):
+                        let imageURL = url
+                        RemoteBugsManager.shared.storeBug((bug.description, imageURL)) { _ in }
+                        print(imageURL)
+                    case .failure:
+                        break
+                    }
+                }
+            case .failure: break
+            }
+        }
+    }
+    
     private func convertImageToData(_ image: UIImage, completion: @escaping (Result<Data, Error>) -> Void) {
         guard let imageData = image.jpegData(compressionQuality: 0.8) else {
             completion(.failure(NSError(domain: "com.example.app", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to convert image to data"])))
