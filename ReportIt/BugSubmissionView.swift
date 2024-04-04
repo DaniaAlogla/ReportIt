57b6844081fa1f4b961f43e35b45d35f697ecacc
DaniaAlogla <daniaalogla@gmail.com>
2024-04-04 21:13:36 +0300

    create 'BugSubmissionViewModel' that will be repponsible for submit a bug using 'RemoteBugManager'

--- ReportIt/BugSubmissionView.swift
+++ ReportIt/BugSubmissionView.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 
+public class BugSubmissionViewModel: ObservableObject {
+}
+
 struct BugSubmissionView: View {
     
     @State private var bug = Bug(description: "", image: nil)
