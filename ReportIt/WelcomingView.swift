d06b8a49986796e0059b9b10b8f2ca00765354d7
DaniaAlogla <daniaalogla@gmail.com>
2024-04-04 19:09:42 +0300

    create a welcoming view that will allow the user to navigate to the bug submission view.

---
+++ ReportIt/WelcomingView.swift
@@ -0,0 +1,37 @@
+//
+//  WelcomingView.swift
+//  ReportIt
+//
+//  Created by Dania Alogla on 25/09/1445 AH.
+//
+
+import SwiftUI
+
+struct WelcomingView: View {
+    
+    @State var showBugSubmissionView = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Image(systemName: "ladybug.fill")
+                    .font(.title)
+                
+                Text("Welcome to ReportIt")
+                    .font(.title)
+                    .padding()
+                
+                Button {
+                    showBugSubmissionView.toggle()
+                } label: {
+                    Text("report a bug")
+                }
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    WelcomingView()
+}
