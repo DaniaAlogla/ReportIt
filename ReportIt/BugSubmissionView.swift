993e3543be6b4aa1c01eb9f18239203b37b45f04
DaniaAlogla <daniaalogla@gmail.com>
2024-04-04 19:20:28 +0300

    create a state bug object to save bug data entered by the user on it.

--- ReportIt/BugSubmissionView.swift
+++ ReportIt/BugSubmissionView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct BugSubmissionView: View {
+    
+    @State private var bug = Bug(description: "", image: nil)
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
