42fe5c6f891a145094ac86d18dfde36d7dfd6ed1
DaniaAlogla <daniaalogla@gmail.com>
2024-04-04 19:14:33 +0300

    create BugSubmissionView.

---
+++ ReportIt/BugSubmissionView.swift
@@ -0,0 +1,18 @@
+//
+//  BugSubmissionView.swift
+//  ReportIt
+//
+//  Created by Dania Alogla on 25/09/1445 AH.
+//
+
+import SwiftUI
+
+struct BugSubmissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BugSubmissionView()
+}
