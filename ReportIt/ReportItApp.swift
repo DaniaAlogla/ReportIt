0b2bc59178c91d2fec72d95f6f170bdb3ec46a73
DaniaAlogla <daniaalogla@gmail.com>
2024-04-04 19:06:03 +0300

    Initial Commit

---
+++ ReportIt/ReportItApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReportItApp.swift
+//  ReportIt
+//
+//  Created by Dania Alogla on 25/09/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ReportItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
