8462e75d73728e416a2a4664e3e1d780b87ddaa4
DaniaAlogla <daniaalogla@gmail.com>
2024-04-24 16:36:18 +0300

    Adding widget extension.

---
+++ ReportItWidgetExtension/ReportItWidgetExtensionBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ReportItWidgetExtensionBundle.swift
+//  ReportItWidgetExtension
+//
+//  Created by Dania Alogla on 15/10/1445 AH.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ReportItWidgetExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        ReportItWidgetExtension()
+    }
+}
