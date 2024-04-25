fc115f2295496c26c3ddd8972eb2d3489a495e64
DaniaAlogla <daniaalogla@gmail.com>
2024-04-25 07:00:57 +0300

    Create 'LocalBug' struct that will hold the bug objects in the cache.

---
+++ ReportIt/Bugs Cache/LocalBug.swift
@@ -0,0 +1,15 @@
+//
+//  LocalBug.swift
+//  ReportIt
+//
+//  Created by Dania Alogla on 16/10/1445 AH.
+//
+
+import Foundation
+
+public struct LocalBug {
+    public let id: UUID
+    public let imageURL: URL
+    public let description: String
+    public let createdOn: Date
+}
