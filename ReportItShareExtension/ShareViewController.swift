629de64ca8cb5548d21a01970bd9ff9b819ffdde
DaniaAlogla <daniaalogla@gmail.com>
2024-04-25 06:45:51 +0300

    Create a special bug submission view for the share extension to provide the ability to close the sheet.

--- ReportItShareExtension/ShareViewController.swift
+++ ReportItShareExtension/ShareViewController.swift
@@ -29,7 +29,7 @@ class ShareViewController: UIViewController {
             
             if let image = providedImage as? UIImage {
                 DispatchQueue.main.async {
-                    let contentView = UIHostingController(rootView: BugSubmissionView(image: image))
+                    let contentView = UIHostingController(rootView: BugSubmissionShareExtensionView(image: image))
                     self.addChild(contentView)
                     self.view.addSubview(contentView.view)
                     
@@ -39,10 +39,33 @@ class ShareViewController: UIViewController {
                     contentView.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
                     contentView.view.rightAnchor.constraint (equalTo: self.view.rightAnchor).isActive = true
                 }
+            } else if let url = providedImage as? URL {
+                if let imageData = try? Data(contentsOf: url) {
+                    if let image = UIImage(data: imageData) {
+                        DispatchQueue.main.async {
+                            let contentView = UIHostingController(rootView: BugSubmissionShareExtensionView(image: image))
+                            self.addChild(contentView)
+                            self.view.addSubview(contentView.view)
+                            
+                            contentView.view.translatesAutoresizingMaskIntoConstraints = false
+                            contentView.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+                            contentView.view.bottomAnchor.constraint (equalTo: self.view.bottomAnchor).isActive = true
+                            contentView.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+                            contentView.view.rightAnchor.constraint (equalTo: self.view.rightAnchor).isActive = true
+                        }
+                    } else {
+                        self.closeShareExtension()
+                        return
+                    }
+                } else {
+                    self.closeShareExtension()
+                    return
+                }
             } else {
                 self.closeShareExtension()
                 return
             }
+        }
             
         }
     }
